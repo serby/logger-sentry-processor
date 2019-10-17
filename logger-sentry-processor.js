c33a0b5644b93697d9157dce2f9b164d39706be6
Paul Serby <paul.serby@clock.co.uk>
2019-10-17 09:03:29 +0100

    feat: Send logger scope to sentry

--- logger-sentry-processor.js
+++ logger-sentry-processor.js
@@ -3,12 +3,13 @@ const createSentryProcessor = sentry => {
 
   return ({ date, pid, scope, level, tags }, ...args) => {
     const [first, ...rest] = args
-    sentry.withScope(scope => {
-      scope.setLevel(level)
+    sentry.withScope(sentryScope => {
+      sentryScope.setLevel(level)
       if (tags) {
-        Object.keys(tags).forEach(tag => scope.setTag(tag, tags[tag]))
+        Object.keys(tags).forEach(tag => sentryScope.setTag(tag, tags[tag]))
       }
-      scope.setExtra('data', rest)
+      sentryScope.setExtra('data', rest)
+      if (scope) sentryScope.setExtra('scope', scope)
       if (first instanceof Error) {
         sentry.captureException(first)
       } else {
